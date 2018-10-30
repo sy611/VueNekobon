46a68995ba44fc47b8a7a3217448ec1eedfcb939
Shun Tomoyose <>
2018-10-30 14:09:30 +0900

    P.73まで終わった

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    list: [
+      { id: 1, name: 'スライム', hp: 100},
+      { id: 2, name: 'ゴブリン', hp: 200},
+      { id: 3, name: 'ドラゴン', hp: 500},
+    ]
+  }
+  
+});
