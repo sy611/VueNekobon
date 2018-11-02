354b3e80caaef27192d0efcb49b0141247499806
Shun Tomoyose <>
2018-11-02 23:52:56 +0900

    P.94まで終了 Chapter2 Clear!!

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 40},
-      { id: 2, name: 'ゴブリン', hp: 200},
-      { id: 3, name: 'ドラゴン', hp: 500},
-    ]
-  },
-  // created: function() {
-  //   this.list.forEach(function(item) {
-  //     this.$set(item, 'active', false);
-  //   }, this);
-  // },
-  methods: {
-    doAttack: function(index) {
-      console.log(index);
-      this.list[index].hp -= 10;
-    }
+    message: 'Hello <strong>Vue.js!</strong>'
   }
   
 });
