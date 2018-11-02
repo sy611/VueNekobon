e7747534b6ef77bfbec72f609fed8222635f6786
Shun Tomoyose <>
2018-11-02 22:57:05 +0900

    P.84まで終わった

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    name: 'キマイラ',
     list: [
-      { id: 1, name: 'スライム', hp: 100},
+      { id: 1, name: 'スライム', hp: 40},
       { id: 2, name: 'ゴブリン', hp: 200},
       { id: 3, name: 'ドラゴン', hp: 500},
     ]
   },
-  created: function() {
-    this.list.forEach(function(item) {
-      this.$set(item, 'active', false);
-    }, this);
-  },
+  // created: function() {
+  //   this.list.forEach(function(item) {
+  //     this.$set(item, 'active', false);
+  //   }, this);
+  // },
   methods: {
-    doAdd: function() {
-      var max = this.list.reduce(function(a, b) {
-        return a > b.id ? a: b.id;
-      }, 0);
-      this.list.push({
-        id: max + 1,
-        name: this.name,
-        hp: 500
-      });
-    },
-    doRemove: function(index) {
-      this.list.splice(index, 1);
-    },
-    addKing: function() {
-      this.$set(this.list, 0, { id: 1, name: 'キングスライム', hp: 500});
+    doAttack: function(index) {
+      console.log(index);
+      this.list[index].hp -= 10;
     }
   }
   
